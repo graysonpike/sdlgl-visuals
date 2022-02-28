8eaa29aa35d8d0ba6aa9e5b7b40c3636df81c29d
Grayson Pike <graysonpike@gmail.com>
2022-02-28 00:48:44 -0500

    Update to use Context and Scene

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 #include "sdlgl/graphics/graphics.h"
 #include <sdlgl/game/clock.h>
 #include <sdlgl/game/scene.h>
-#include <sdlgl/game/simple_context.h>
 #include <sdlgl/input/inputs.h>
 #include <stdlib.h>
 #include <time.h>
 #include <cstdlib>
 
+#include <sdlgl/game/scene.h>
+#include <sdlgl/game/context.h>
 #include <sdlgl/ui/fps_display.h>
 #include <sdlgl/ui/entity_count.h>
 #include <sdlgl/utilities/noise.h>
@@ -19,14 +20,14 @@
 #define KEY_TOGGLE_DEBUG_VISUALS SDL_SCANCODE_GRAVE // Tilde
 
 
-void game_loop(SimpleContext context) {
+void game_loop(Context context, Scene *scene) {
 
     context.inputs->update();
     context.clock->tick();
     context.graphics->clear_screen({255, 255, 255, 255});
 
-    context.scene->update(context.clock->get_delta());
-    context.scene->render();
+    scene->update(context.clock->get_delta());
+    scene->render();
 
     if (context.inputs->is_key_down_event(KEY_TOGGLE_DEBUG_VISUALS)) {
         context.graphics->toggle_debug_visuals();
@@ -51,25 +52,25 @@ int main() {
 
     PerlinNoise::init();
 
-    SimpleContext context(new Graphics(1920, 1080), new Inputs(), new Clock());
+    Context context(new Graphics(1920, 1080), new Audio(), new Inputs(), new Clock());
 
     // Load resources
     context.graphics->get_resources()->load_resources("resources.json");
 
     // Create and populate scene
-    context.scene = new Scene(context.inputs, context.graphics);
+    Scene *scene = new Scene(context.graphics, context.audio, context.inputs);
 
     // UI entities
-    context.scene->add_entity(new FPS_Display(
-        context.scene, "base_text", {0, 0, 0, 255}));
-    context.scene->add_entity(new EntityCount(
-        context.scene, "base_text", {0, 0, 0, 255}));
+    scene->add_entity(new FPS_Display(
+        scene, "base_text", {0, 0, 0, 255}));
+    scene->add_entity(new EntityCount(
+        scene, "base_text", {0, 0, 0, 255}));
 
-    context.scene->add_entity(new Grid(context.scene, -160, 120, 30*32, 30*16, 30));
+    scene->add_entity(new Grid(scene, -160, 120, 30*32, 30*16, 30));
 
     while (*context.loop) {
 
-        game_loop(context);
+        game_loop(context, scene);
 
     }
 
